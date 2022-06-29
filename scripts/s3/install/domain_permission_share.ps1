509488e43d7bef755b3dd2aa5f526810dcf70d5c
suportekxc <higor.fmonteiro@gmail.com>
2022-06-29 18:21:42 -0300

    code cleaning

--- scripts/s3/install/domain_permission_share.ps1
+++
@@ -1,4 +0,0 @@
-# Permission to example Service user
-
-New-SmbShare -Path "D:\exampleShare" -Name "exampleShare" -fullaccess "example.LOCAL\exampleservice"
-Grant-SmbShareAccess -Name "exampleShare" -AccountName "example.LOCAL\exampleservice" -AccessRight Full -Force
--- scripts/s3/update/qlik-update.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Initiating silent example Sense Update..."
-
-Start-Process -FilePath "C:\Users\Administrator\example_Sense_Update.exe" -ArgumentList "install" -NoNewWindow -Wait -Passthru
-
-Start-Sleep -Seconds 120 ; Restart-Computer -Force
